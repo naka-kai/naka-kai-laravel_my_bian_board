08afb59b7a55357dbe0374cd18c07324784965e1
kai <sea.k.naka@gmail.com>
2022-02-12 19:18:55 +0900

    message accordion finish

--- public/js/messageAcd.js
+++ public/js/messageAcd.js
@@ -3,6 +3,16 @@ var __webpack_exports__ = {};
 /*!************************************!*\
   !*** ./resources/js/messageAcd.js ***!
   \************************************/
-console.log('ok');
+$(function () {
+  if (sessionStorage.getItem('msg_acd') == 'block') {
+    $('#msg_acd').show();
+  }
+
+  $('#msg_btn').on("click", function () {
+    $(msg_acd).slideToggle('fast', function () {
+      sessionStorage.setItem($(msg_acd).attr('id'), $(msg_acd).css('display'));
+    });
+  });
+});
 /******/ })()
 ;
--- resources/js/messageAcd.js
+++ resources/js/messageAcd.js
@@ -1,1 +1,12 @@
-console.log('ok');
+
+$(function () {
+    if (sessionStorage.getItem('msg_acd') == 'block') {
+        $('#msg_acd').show();
+    }
+
+    $('#msg_btn').on("click", function () {
+        $(msg_acd).slideToggle('fast', function () {
+            sessionStorage.setItem($(msg_acd).attr('id'), $(msg_acd).css('display'));
+        });
+    });
+});
