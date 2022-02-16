5b87d6639b3f81653d5a80265181c470f9e61547
kai <sea.k.naka@gmail.com>
2022-02-16 18:42:06 +0900

    add @routes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
-        {{-- @routes --}}
+        @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
